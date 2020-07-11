1053850ad1154fe3caee9d219e582d74708f09d9
Ilja Daderko <idaderko@gmail.com>
2020-07-11 19:12:06 +0300

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare module "yoga-dom" {
+  export {
+    default,
+    YogaAlign,
+    YogaConfig,
+    YogaDirection,
+    YogaDisplay,
+    YogaEdge,
+    YogaExperimentalFeature,
+    YogaFlexDirection,
+    YogaFlexWrap,
+    YogaJustifyContent,
+    YogaNode,
+    YogaOverflow,
+    YogaPositionType,
+    YogaUnit,
+  } from "yoga-layout";
+}
